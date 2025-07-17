6582c99157dc1070819e2a5913b3bb2ab98781c7
ArnavTicku <arnavticku01@gmail.com>
2025-07-18 04:28:37 +0530

    middlewares

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer"
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+
+        cb(null, file.originalname)
+    }
+}) // these all are parameters
+// cb is basically callback or returning values in simple terms...
+export const upload = multer({ storage: storage })
